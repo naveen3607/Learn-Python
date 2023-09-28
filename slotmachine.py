0443722ae8fa0cce720cd4bc9b7231cbd13f4415
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-28 15:49:45 -0400

    Python Script

--- slotmachine.py
+++ slotmachine.py
@@ -7,6 +7,33 @@ MIN_BET = 1
 ROWS = MAX_LINES
 COLS = MAX_LINES
 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.item():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 def deposit():
     while True:
