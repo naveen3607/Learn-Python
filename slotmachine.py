9d124c0b6c7edbc773cbc6e4b779f3c81456fd54
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-28 15:37:03 -0400

    Python Script

--- slotmachine.py
+++ slotmachine.py
@@ -1,6 +1,11 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+
 def deposit():
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -12,8 +17,46 @@ def deposit():
     return amount
 
 
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter the valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def get_bet():
+    while True:
+        bet_amount = input("What would you like to bet? $ ")
+        if bet_amount.isdigit():
+            bet_amount = int(bet_amount)
+            if MIN_BET <= bet_amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return bet_amount
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet_amount = get_bet()
+        total_bet_amount = bet_amount * lines
+        if total_bet_amount > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance} ")
+        else:
+            break
+
+    print(f"You are betting ${bet_amount} on {lines} lines. Total bet is equal to ${total_bet_amount}")
 
 
 main()
