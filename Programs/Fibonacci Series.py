b36c9fa8927ef21acb75f124c099aa7360f094ef
naveen3607 <naveenreddykallam3607@gmail.com>
2023-11-10 15:32:31 -0500

    Fibonacci Series

---
+++ Programs/Fibonacci Series.py
@@ -0,0 +1,14 @@
+# Fibonacci Series
+n = int(input("Enter the total number of values in the series, n"))
+
+
+def fibonacci(n: int):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(f"The sum of the two preceding numbers is", fibonacci(n))
